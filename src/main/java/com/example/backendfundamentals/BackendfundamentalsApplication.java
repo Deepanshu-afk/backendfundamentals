6aa100829421f3d735cefedb10bc3901fb783b7c
deepanshu-afk <deepanshu.bhardwaj@cleaff.com>
2025-09-10 16:56:05 +0530

    add RestAPIsPrinciple class

---
+++ src/main/java/com/example/backendfundamentals/BackendfundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendfundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendfundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendfundamentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendfundamentals/BackendfundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendfundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendfundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
